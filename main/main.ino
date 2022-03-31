2a431e1e4f05ceb3a83b31fbd6390032d30ca663
Sutthikan panla <sutigran2557@gmail.com>
2022-03-31 13:37:05 +0700

    add motor

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <IBusBM.h>
+#include <CytronMotorDriver.h>
 
 int voltage = 0;
 int RA = 0;
@@ -11,8 +12,13 @@ int RA_pin = 20;
 int CE_pin = 7;
 int CSN_pin = 8;
 
+int A1_pin = 10;
+int A2_pin = 11;
+int B1_pin = 12;
+int B2_pin = 13;
+
 const uint64_t address = 0xF0F0F0F0E1LL;
-HardwareSerial& RCibus = Serial1;
+HardwareSerial &RCibus = Serial1;
 
 struct Mydata
 {
@@ -21,6 +27,8 @@ struct Mydata
   byte connection;
 };
 
+CytronMD M1(PWM_PWM, A1_pin, A2_pin);
+CytronMD M2(PWM_PWM, B1_pin, B2_pin);
 RF24 radio(CE_pin, CSN_pin); // CE, CSN
 Mydata data;
 
@@ -31,7 +39,6 @@ void setup()
   pinMode(RA_pin, INPUT);
   radioactive_setup(RA_pin);
   ibusRC_setup(RCibus);
-
 }
 
 void loop()
@@ -47,6 +54,10 @@ void loop()
     radio.write(&data, sizeof(Mydata));
   }
   delay(10);
+}
+
+void remote_contorl()
+{
 }
 
 void Serial_test()
